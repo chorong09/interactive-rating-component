01335ff6372f807755190c717a572d3de6b09525
chorong09 <149172968+chorong09@users.noreply.github.com>
2023-11-29 14:20:21 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function onClick() {
+  btn.style.backgroundColor = 'hsl(25, 97%, 53%)';
+  btn.style.color = 'white';
+});
+
+document.querySelector()
